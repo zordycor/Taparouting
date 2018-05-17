57d7dee453628e44b6bbda7d72488d58f6d4435c
Borja Rodrigo Martí <Borja@MacBook-Air-de-Borja-166.local>
2018-05-18 00:19:14 +0200

    color y search navbar, img carousel y calendario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="es"><head>
+<html lang="es">
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -8,17 +9,24 @@
     <title>Taparouting</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Local CSS -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+  <!-- Animations -->
+  <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  
   </head>
 
   <body>
 
-    <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+    <nav class="navbar navbar-expand-md navbar-red navbar-light fixed-top">
       <a class="navbar-brand" href="#">Taparouting</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -36,23 +44,40 @@
             <a class="nav-link" href="#">Rutas</a>
           </li>
         </ul>
-          <button class="btn btn-outline-success my-2 my-sm-0">Iniciar sesión</button>
+        <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search">
+    </form>
+          <button class="btn btn-outline-warning my-2 my-sm-0">Iniciar sesión</button>
       </div>
     </nav>
 
-    <main role="main">
 
       <!-- Main jumbotron for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <div class="container">
-          <h1 class="display-3">Bienvenido!</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+
+      <div class="main-container">
+            <div class="welcome">
+              <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block img-fluid" src="http://silviaquirosblog.com/wp-content/uploads/2014/11/TapasenBarcelona.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block img-fluid" src="http://blog.pepebar.com/wp-content/uploads/2017/02/tapas-y-pinchos-bar.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block img-fluid" src="https://img.grouponcdn.com/iam/kYEnGV2g2ZEcxj3pRfav/UT-5000x3000/v1/c700x420.jpg" alt="Third slide">
+                </div>
+              </div>
+            </div>
+          </div>
+            <ul class="calendar">
+                <li>18/05 Ruta de la tapa <strong>Benetusser</strong></li>
+                <li>23/07 Ruta de la tapa <strong>Puzol</strong></li>
+            </ul>
         </div>
       </div>
 
       <div class="container">
-        <!-- Example row of columns -->
         <div class="row">
           <div class="col-md-4">
             <h2>Heading</h2>
@@ -73,21 +98,8 @@
 
         <hr>
 
-      </div> <!-- /container -->
-
-    </main>
-
     <footer class="container">
       <p>© Taparouting 2017-2018</p>
     </footer>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../../../dist/js/bootstrap.min.js"></script>
-  
-
+    
 </body></html>
