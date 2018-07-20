5e1904a871d0f373f25946eeb2c97ffa2fd876a6
Borja Rodrigo <zordycorak@gmail.com>
2018-07-20 10:05:14 +0200

    Marker on barMap. barMap styling.

--- resources/views/layouts/bar.blade.php
+++ resources/views/layouts/bar.blade.php
@@ -1,34 +1,42 @@
 @extends('templates.master')
 @section('main')
 
-    <div class="main-container animate fade-slide-down contBar">
-        <a href=""><i class="fas fa-angle-left"></i></a>
-            <img class="tapaimg barView" src="/img/{{ $bar->tapaimg }}" alt="">
-            <div class="row rowText">
-                <div class="title">
-                    <h2>{{$bar->tapanom}}</h2>
-                    <h4><a href="/ruta/{{ $ruta->localidad }}">{{ $ruta->nombre }}</a></h4>
+<div class="cont contBar">
+    <a href="/"><i class="fas fa-angle-left"></i></a>
+    <div class="firstBlock">
+        <div class="bg fade-slide-left"></div>
+        <div class="row">
+            <div class="bg-row"></div>
+            <div class="title">
+                <h1>
+                    {{ $bar->tapanom }}
+                </h1>
+                <h4>{{$ruta->nombre}}</h4>
+                <div class="rowData">
+                    <div class="datosBar">
+                        <h3>{{ $bar->nombre }}</h3>
+                        <h3>{{ $bar->direccion }}</h3>
+                        <h4>{{ $bar->horarios }}</h4>
+                    </div>
                 </div>
             </div>
-
-            <div class="row rowData">
-                <div class="datosBar">
-                    <h4>{{ $bar->nombre }}</h4>
-                    <h4>{{ $bar->direccion }}</h4>
-                    <h4>{{ $bar->horarios }}</h4>
-                </div>
-            </div>
-        <div class="descMap">
-            <div id="map_canvas"></div>
         </div>
+
+        <div class="imageData">
+            <div class="description">
+                {{ $bar->tapadesc }}
+            </div>
+            <img class="tapaimg barView" src="/img/{{ $bar->tapaimg }}" alt="">
+        </div>
+        <div id="map_canvas"></div>
     </div>
 
-
 <script>
+
     function mapInit() {
         var address = '{{ $bar->nombre }}, {{ $bar->direccion }}';
-
         var geocoder = new google.maps.Geocoder();
+
         geocoder.geocode({
             'address': address
         }, function(results, status) {
@@ -36,12 +44,17 @@
                 var Lat = results[0].geometry.location.lat();
                 var Lng = results[0].geometry.location.lng();
                 var myOptions = {
-                    zoom: 15,
+                    zoom: 18,
                     center: new google.maps.LatLng(Lat, Lng)
                 };
 
                 var map = new google.maps.Map(
                     document.getElementById("map_canvas"), myOptions);
+
+                var marker = new google.maps.Marker({
+                    position: myOptions['center'],
+                    map: map
+                });
 
             } else {
                 alert("Something got wrong " + status);
